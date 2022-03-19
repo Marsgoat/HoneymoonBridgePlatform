095588de4882647716ddd7b734c6cfb9f98035ce
Marsgoat <johnny3681@gmail.com>
2022-03-19 21:39:32 +0800

    common js

--- src/game.js
+++ src/game.js
@@ -7,26 +7,25 @@ const CLUB = 3;
  * 洗牌 Fisher-Yates shuffle
  * @param {Array}} 牌組陣列
  */
-// function shuffle(deck) {
-//   for (let i = deck.length - 1; i > 0; i--) {
-//     const r = Math.floor(Math.random() * (i + 1));
-//     [deck[i], deck[r]] = [deck[r], deck[i]];
-//   }
-// }
-exports.shuffle = (deck) => {
+function shuffle(deck) {
   for (let i = deck.length - 1; i > 0; i--) {
     const r = Math.floor(Math.random() * (i + 1));
     [deck[i], deck[r]] = [deck[r], deck[i]];
   }
-};
+}
 
-exports.createDeck = () => {
+function createDeck() {
   const deck = [];
   for (let i = 0; i < 4; i++) {
-    for (let j = 1; j < 14; j++) {
+    for (let j = 0; j < 13; j++) {
       deck.push({ suit: i, value: j });
     }
   }
-  exports.shuffle(deck);
+  shuffle(deck);
   return deck;
+}
+
+module.exports = {
+  shuffle,
+  createDeck,
 };
