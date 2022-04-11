b86455a280b0811572855a99bff0f4c2a54ef344
Marsgoat <johnny3681@gmail.com>
2022-04-11 23:23:22 +0800

    ready-to-show

--- src/main.js
+++ src/main.js
@@ -1,16 +1,23 @@
-const { app, BrowserWindow } = require('electron');
+const { sign } = require('crypto');
+const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
+    show: false,
   });
 
   win.loadFile('./src/index.html');
+
+  // 畫面載入完成後才顯示畫面
+  win.once('ready-to-show', () => {
+    win.show();
+  });
 }
 
 app.whenReady().then(() => {
